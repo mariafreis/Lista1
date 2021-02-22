961b8798d64d04a5b5530223e7eecff4f3f82b1d
Maria Fernanda Moge dos Reis <70169979+mafereis@users.noreply.github.com>
2021-02-22 20:49:16 -0300

    Update TestaCarro.java
    ORIENTANDO AO OBJETO

--- src/main/java/br/com/unifacef/lista1/TestaCarro.java
+++ src/main/java/br/com/unifacef/lista1/TestaCarro.java
@@ -5,46 +5,33 @@
  */
 package br.com.unifacef.lista1;
 
-import javax.swing.JOptionPane;
-
-/**
- *
- * @author ferdi
- */
 public class TestaCarro {
 
-    /** 
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        // Vamos criar objetos aqui
-        //Obj 1
-        //Tipo nome = New Tipo()
+    
+public static void main(String[] args) {
+
+// vamos criar os 3 objetos aqui
+        // obj1
+        // Tipo nome = new Tipo()
         Carros obj1 = new Carros();
-        //Obj.variavel
+        // objeto.variavel
+        obj1.ano = 2018;
         obj1.marca = "Chevrolet";
         obj1.modelo = "Onix";
-        obj1.ano = 2018;
         obj1.motor = false;
         obj1.velAtual = 0;
-//Vamos mostrar ao usuario
-        System.out.println("Obj1: Ano: " + obj1.ano + " Marca: " + obj1.marca + " Modelo: " + obj1.modelo
-                + " Motor: " + obj1.motor + " Velocidade: " + obj1.velAtual);
-        JOptionPane.showMessageDialog(null, "Obj1: Ano: " + obj1.ano + " Marca: " + obj1.marca + " Modelo: " + obj1.modelo
-                + " Motor: " + obj1.motor + " Velocidade: " + obj1.velAtual);
+        // vamos mostrar ao usuário
 
-        //Obj 2
-        //Tipo nome = New Tipo()
-        Carros obj2 = new Carros();
-        //Obj.variavel
-        obj2.marca = "Fiat";
-        obj2.modelo = "Palio";
-        obj2.ano = 2016;
-        obj2.motor = true;
-        obj2.velAtual = 40;
-        System.out.println("Obj2: Ano: " + obj2.ano + " Marca: " + obj2.marca + " Modelo: " + obj2.modelo
-                + " Motor: " + obj2.motor + " Velocidade: " + obj2.velAtual);
+        obj1.mostra();
+        obj1.mostraPane();
 
+        // criar um objeto significa chamar seu método construtor
+        Carros obj2 = new Carros(2016, "Fiat", "Palio", true, 40);
+        obj2.mostra();
+        obj2.mostraPane();
+
+        Carros obj3 = new Carros(2021, "Farrari", "Sport 6", false, 0);
+        obj3.mostra();
+        obj3.mostraPane();
     }
 }
